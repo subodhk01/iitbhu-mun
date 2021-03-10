d5dc85048196d0a99596cfa3f29da921422bd5b2
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2021-03-10 21:12:10 +0530

    js_edit

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $(function() {
             init = function() {
 
                 initEvents();
-                
+
             },
             initEvents = function() {
 
@@ -43,30 +43,30 @@ $(function() {
                 } );
 
                 $navArrows.children( ':first' ).on( 'click', function() {
-                    
+
                     slitslider.previous();
                     return false;
 
                 } );
 
                 $nav.each( function( i ) {
-                
+
                     $( this ).on( 'click', function( event ) {
-                        
+
                         var $dot = $( this );
-                        
+
                         if( !slitslider.isActive() ) {
 
                             $nav.removeClass( 'nav-dot-current' );
                             $dot.addClass( 'nav-dot-current' );
-                        
+
                         }
-                        
+
                         slitslider.jump( i + 1 );
                         return false;
-                    
+
                     } );
-                    
+
                 } );
 
             };
@@ -101,7 +101,7 @@ $(document).ready(function(){
 			console.log('done scrolling');
 		}
 	});
-	
+
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
             $(".navbar-brand a").css("color","#fff");
@@ -111,23 +111,23 @@ $(document).ready(function(){
             $("#navigation").addClass("animated-header");
         }
     });
-	
+
 	/* ========================================================================= */
 	/*	Fix Slider Height
-	/* ========================================================================= */	
+	/* ========================================================================= */
 
     // Slider Height
     var slideHeight = $(window).height();
-    
+
     $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
 
     $(window).resize(function(){'use strict',
         $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
     });
-	
-	
-	
-	$("#works, #testimonial").owlCarousel({	 
+
+
+
+	$("#works, #testimonial").owlCarousel({
 		navigation : true,
 		pagination : false,
 		slideSpeed : 700,
@@ -135,8 +135,8 @@ $(document).ready(function(){
 		singleItem:true,
 		navigationText: ["<i class='fa fa-angle-left fa-lg'></i>","<i class='fa fa-angle-right fa-lg'></i>"]
 	});
-	
-	
+
+
 	/* ========================================================================= */
 	/*	Featured Project Lightbox
 	/* ========================================================================= */
@@ -151,15 +151,15 @@ $(document).ready(function(){
 		closeSpeed  : 550,
 
 		closeClick : true,
-			
+
 		beforeShow: function () {
 			this.title = $(this.element).attr('title');
 			this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
 		},
-		
+
 		helpers : {
-			title : { 
-				type: 'inside' 
+			title : {
+				type: 'inside'
 			},
 			overlay : {
 				css : {
@@ -168,7 +168,7 @@ $(document).ready(function(){
 			}
 		}
 	});
-	
+
 });
 
 
@@ -193,7 +193,7 @@ function init() {
 	        scaleControl: false,
 	        draggable: true,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             featureType: 'water',
@@ -244,7 +244,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map-canvas');
 
@@ -266,4 +266,3 @@ var wow = new WOW ({
 	mobile:       false,       // trigger animations on mobile devices (default is true)
 });
 wow.init();
-
